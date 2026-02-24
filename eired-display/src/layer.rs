9e8c28e447faadefcda9f4d29f736805ad867f77
TundraClimate <tundra.tundere@gmail.com>
2026-02-24 15:52:22 +0900

    📝docs: Add docs for layer.rs

--- eired-display/src/layer.rs
+++ eired-display/src/layer.rs
@@ -5,6 +5,28 @@ use crate::{Annot, Annotate, Span};
 
 #[derive(Default, PartialEq, Eq)]
 /// A layer of merged spans.
+///
+/// Includes spans has annotated for hold coords of relative on terminal.
+/// Spans only uses to push a [Canvas](crate::Canvas), may changes coords and size in always.
+///
+/// # Examples
+///
+/// ```
+/// # use eired_display::Layer;
+/// use eired_display::Annotate;
+/// use eired_display::Span;
+///
+/// let mut layer = Layer::default();
+///
+/// // Square
+/// layer.push_span_write(Span::from("XXX").annotate((0, 0)));
+/// layer.push_span_write(Span::from("XOX").annotate((0, 1)));
+/// layer.push_span_write(Span::from("XXX").annotate((0, 2)));
+///
+/// assert!(layer.take_with(|s| s.inner() == &Span::from("XXX")).is_some());
+/// assert!(layer.take_with(|s| s.inner() == &Span::from("XOX")).is_some());
+/// assert!(layer.take_with(|s| s.inner() == &Span::from("XXX")).is_some());
+/// ```
 pub struct Layer {
     width: u16,
     height: u16,
@@ -13,6 +35,24 @@ pub struct Layer {
 
 impl Layer {
     /// Get inner slice.
+    ///
+    /// Slice not sorted.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use eired_display::Layer;
+    /// use eired_display::Annotate;
+    /// use eired_display::Span;
+    ///
+    /// let mut layer = Layer::default();
+    ///
+    /// layer.push_span_write(Span::from("XXX").annotate((0, 0)));
+    /// layer.push_span_write(Span::from("XOX").annotate((0, 1)));
+    /// layer.push_span_write(Span::from("XXX").annotate((0, 2)));
+    ///
+    /// assert_eq!(layer.inner().len(), 3);
+    /// ```
     pub fn inner(&self) -> &[Annot<Span>] {
         &self.spans
     }
@@ -32,9 +72,9 @@ impl Layer {
         }
 
         let (overlap_begin, overlap_end) = (overlap.base_pos().0, overlap.outer_apex_pos().0);
-        let is_include_begin = base.contains_pos(overlap_begin, 0);
-        let is_include_end = base.contains_pos(overlap_end - 1, 0);
         let (base_x, base_y) = base.base_pos();
+        let is_include_begin = base.contains_pos(overlap_begin, base_y);
+        let is_include_end = base.contains_pos(overlap_end - 1, base_y);
         let mut solved = vec![];
 
         match (is_include_begin, is_include_end) {
@@ -68,10 +108,39 @@ impl Layer {
             (false, false) => {}
         }
 
+        debug_assert!(!solved.is_empty(), "unsolved conflict");
+
         solved.into_iter().flatten().collect()
     }
 
-    /// Pushes span that overlap other spans.
+    /// Pushes span that overlaps to above.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use eired_display::Layer;
+    /// use eired_display::Annotate;
+    /// use eired_display::Span;
+    ///
+    /// let mut layer = Layer::default();
+    ///
+    /// // Square
+    /// layer.push_span_write(Span::from("XXX").annotate((0, 0)));
+    /// layer.push_span_write(Span::from("XXX").annotate((0, 1)));
+    /// layer.push_span_write(Span::from("XXX").annotate((0, 2)));
+    ///
+    /// // Above square
+    /// layer.push_span_write(Span::from("OOO").annotate((1, 1)));
+    /// layer.push_span_write(Span::from("OOO").annotate((1, 2)));
+    /// layer.push_span_write(Span::from("OOO").annotate((1, 3)));
+    ///
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("XXX")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("X")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("OOO")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("X")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("OOO")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("OOO")).is_some());
+    /// ```
     pub fn push_span_write(&mut self, span: Annot<Span>) {
         if span.is_empty() {
             return;
@@ -97,7 +166,34 @@ impl Layer {
         self.push_span(span);
     }
 
-    /// Pushes size fixed span by other spans.
+    /// Pushes span that overlaps to below.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use eired_display::Layer;
+    /// use eired_display::Annotate;
+    /// use eired_display::Span;
+    ///
+    /// let mut layer = Layer::default();
+    ///
+    /// // Square
+    /// layer.push_span_fixed(Span::from("XXX").annotate((0, 0)));
+    /// layer.push_span_fixed(Span::from("XXX").annotate((0, 1)));
+    /// layer.push_span_fixed(Span::from("XXX").annotate((0, 2)));
+    ///
+    /// // Above square
+    /// layer.push_span_fixed(Span::from("OOO").annotate((1, 1)));
+    /// layer.push_span_fixed(Span::from("OOO").annotate((1, 2)));
+    /// layer.push_span_fixed(Span::from("OOO").annotate((1, 3)));
+    ///
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("XXX")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("XXX")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("O")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("XXX")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("O")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("OOO")).is_some());
+    /// ```
     pub fn push_span_fixed(&mut self, span: Annot<Span>) {
         if span.is_empty() {
             return;
@@ -146,6 +242,31 @@ impl Layer {
     }
 
     /// Pushes span if not conflict other spans.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use eired_display::Layer;
+    /// use eired_display::Annotate;
+    /// use eired_display::Span;
+    ///
+    /// let mut layer = Layer::default();
+    ///
+    /// // Square
+    /// layer.push_span_only_valid(Span::from("XXX").annotate((0, 0)));
+    /// layer.push_span_only_valid(Span::from("XXX").annotate((0, 1)));
+    /// layer.push_span_only_valid(Span::from("XXX").annotate((0, 2)));
+    ///
+    /// // Above square
+    /// layer.push_span_only_valid(Span::from("OOO").annotate((1, 1)));
+    /// layer.push_span_only_valid(Span::from("OOO").annotate((1, 2)));
+    /// layer.push_span_only_valid(Span::from("OOO").annotate((1, 3)));
+    ///
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("XXX")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("XXX")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("XXX")).is_some());
+    /// assert!(layer.take_with(|s| s.inner() == &Span::from("OOO")).is_some());
+    /// ```
     pub fn push_span_only_valid(&mut self, span: Annot<Span>) {
         if span.is_empty() || self.spans.iter().any(|s| s.is_conflict(&span)) {
             return;
@@ -155,7 +276,36 @@ impl Layer {
         self.push_span(span);
     }
 
-    /// Create overlaps another layer to `self`.
+    /// Create overlapped another layer with `self`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use eired_display::Layer;
+    /// use eired_display::Annotate;
+    /// use eired_display::Span;
+    ///
+    /// let mut below = Layer::default();
+    ///
+    /// below.push_span_write(Span::from("XXX").annotate((0, 0)));
+    /// below.push_span_write(Span::from("XXX").annotate((0, 1)));
+    /// below.push_span_write(Span::from("XXX").annotate((0, 2)));
+    ///
+    /// let mut above = Layer::default();
+    ///
+    /// above.push_span_write(Span::from("OOO").annotate((1, 1)));
+    /// above.push_span_write(Span::from("OOO").annotate((1, 2)));
+    /// above.push_span_write(Span::from("OOO").annotate((1, 3)));
+    ///
+    /// let mut layer = below.overlap((0, 0), above.annotate((0, 0)));
+    ///
+    /// assert!(layer.inner_mut().take_with(|s| s.inner() == &Span::from("XXX")).is_some());
+    /// assert!(layer.inner_mut().take_with(|s| s.inner() == &Span::from("X")).is_some());
+    /// assert!(layer.inner_mut().take_with(|s| s.inner() == &Span::from("OOO")).is_some());
+    /// assert!(layer.inner_mut().take_with(|s| s.inner() == &Span::from("X")).is_some());
+    /// assert!(layer.inner_mut().take_with(|s| s.inner() == &Span::from("OOO")).is_some());
+    /// assert!(layer.inner_mut().take_with(|s| s.inner() == &Span::from("OOO")).is_some());
+    /// ```
     pub fn overlap(&self, self_root: (u16, u16), upper: Annot<Layer>) -> Annot<Layer> {
         let mut new_layer = Layer::default();
         let init_spans = self.spans.to_vec();
@@ -178,6 +328,26 @@ impl Layer {
     }
 
     /// Adds offset to left top.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use eired_display::Layer;
+    /// use eired_display::Annotate;
+    /// use eired_display::Span;
+    ///
+    /// let mut layer = Layer::default();
+    ///
+    /// layer.push_span_write(Span::from("XXX").annotate((0, 0)));
+    /// layer.push_span_write(Span::from("XXX").annotate((0, 1)));
+    /// layer.push_span_write(Span::from("XXX").annotate((0, 2)));
+    ///
+    /// layer.add_offset((3, 3));
+    ///
+    /// assert!(layer.take_with(|s| s == &Span::from("XXX").annotate((3, 3))).is_some());
+    /// assert!(layer.take_with(|s| s == &Span::from("XXX").annotate((3, 4))).is_some());
+    /// assert!(layer.take_with(|s| s == &Span::from("XXX").annotate((3, 5))).is_some());
+    /// ```
     pub fn add_offset(&mut self, offset: (u16, u16)) {
         self.width += offset.0;
         self.height += offset.1;
@@ -188,6 +358,31 @@ impl Layer {
                 *y += offset.1;
             })
         }
+    }
+
+    /// Takes first element of matchs condition.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use eired_display::Layer;
+    /// use eired_display::Annotate;
+    /// use eired_display::Span;
+    ///
+    /// let mut layer = Layer::default();
+    ///
+    /// layer.push_span_write(Span::from("X").annotate((0, 0)));
+    /// layer.push_span_write(Span::from("XX").annotate((0, 1)));
+    /// layer.push_span_write(Span::from("XXX").annotate((0, 2)));
+    /// layer.push_span_write(Span::from("OOOO").annotate((0, 3)));
+    /// layer.push_span_write(Span::from("XXXXX").annotate((0, 4)));
+    ///
+    /// assert_eq!(layer.take_with(|s| s.width() == 4).unwrap().inner(), &Span::from("OOOO"));
+    /// ```
+    pub fn take_with<F: Fn(&Annot<Span>) -> bool>(&mut self, f: F) -> Option<Annot<Span>> {
+        let pos = self.spans.iter().position(f);
+
+        pos.map(|pos| self.spans.swap_remove(pos))
     }
 }
 
