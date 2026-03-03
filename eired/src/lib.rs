bf22c1dc04a6d46ef58e33e4a415a3cd77761d2e
TundraClimate <tundra.tundere@gmail.com>
2026-03-03 11:12:34 +0900

    ✨feat: Add RenderRuntime base

--- eired/src/lib.rs
+++ eired/src/lib.rs
@@ -1,25 +1,27 @@
 use std::io::{self, Write};
+use std::marker::PhantomData;
+
+use crossbeam::channel::{self, Receiver, Sender};
 
 use eired_display::{Annotate, VTerm};
 
 pub trait Renderer<W: Write> {
-    fn render(&mut self, cells: VTerm) -> io::Result<()>;
+    fn render(&mut self, config: &RuntimeConfig, cells: VTerm) -> io::Result<()>;
 }
 
 pub struct TerminalRenderer<W: Write> {
-    base_pos: (u16, u16),
     writer: W,
 }
 
 impl<W: Write> TerminalRenderer<W> {
-    pub fn new(base_pos: (u16, u16), writer: W) -> Self {
-        Self { base_pos, writer }
+    pub fn new(writer: W) -> Self {
+        Self { writer }
     }
 }
 
 impl<W: Write> Renderer<W> for TerminalRenderer<W> {
-    fn render(&mut self, cells: VTerm) -> io::Result<()> {
-        let cmds = eired_display::convert_to_spans(cells.annotate(self.base_pos));
+    fn render(&mut self, config: &RuntimeConfig, cells: VTerm) -> io::Result<()> {
+        let cmds = eired_display::convert_to_spans(cells.annotate(config.base_pos));
 
         for cmd in cmds {
             cmd.draw(&mut self.writer)?;
@@ -30,16 +32,20 @@ impl<W: Write> Renderer<W> for TerminalRenderer<W> {
 }
 
 pub struct RenderOptimizer {
-    prev_cache: VTerm,
+    prev_cache: Option<VTerm>,
 }
 
 impl RenderOptimizer {
     fn replace_cache(&mut self, new_cache: VTerm) {
-        self.prev_cache = new_cache;
+        self.prev_cache = Some(new_cache);
     }
 
     fn create_diff(&self, new_term: &VTerm) -> Option<VTerm> {
-        if self.prev_cache.len() != new_term.len() {
+        let Some(ref prev_cache) = self.prev_cache else {
+            return Some(new_term.clone());
+        };
+
+        if prev_cache.len() != new_term.len() {
             return Some(new_term.clone());
         }
 
@@ -47,7 +53,7 @@ impl RenderOptimizer {
         let mut is_changed = false;
 
         for (i, new_cell) in new_term.iter().enumerate() {
-            if self.prev_cache.get(i) != new_term.get(i) {
+            if prev_cache.get(i) != new_term.get(i) {
                 cells[i] = *new_cell;
 
                 if !is_changed {
@@ -57,5 +63,70 @@ impl RenderOptimizer {
         }
 
         is_changed.then_some(VTerm::new(new_term.width(), new_term.height(), cells))
+    }
+}
+
+impl RenderOptimizer {
+    fn new() -> Self {
+        Self { prev_cache: None }
+    }
+}
+
+pub struct RuntimeConfig {
+    fps: u16,
+    base_pos: (u16, u16),
+}
+
+impl RuntimeConfig {
+    fn get_fps_ms(&self) -> f64 {
+        1.0 / self.fps as f64
+    }
+
+    fn get_base_pos(&self) -> (u16, u16) {
+        self.base_pos
+    }
+}
+
+pub trait RuntimeTask {}
+
+pub struct RenderRuntime<W: Write, R: Renderer<W>> {
+    out: PhantomData<W>,
+    config: RuntimeConfig,
+    renderer: R,
+    optimizer: RenderOptimizer,
+    rx: Receiver<Box<dyn RuntimeTask>>,
+}
+
+impl<W: Write, R: Renderer<W>> RenderRuntime<W, R> {
+    pub fn new(config: RuntimeConfig, renderer: R) -> (Self, Sender<Box<dyn RuntimeTask>>) {
+        let (tx, rx) = channel::bounded(1024);
+
+        let rt = Self {
+            out: PhantomData,
+            config,
+            renderer,
+            optimizer: RenderOptimizer::new(),
+            rx,
+        };
+
+        (rt, tx)
+    }
+
+    pub fn run(mut self) {
+        self.store();
+        self.change_loop();
+        self.restore();
+    }
+
+    fn change_loop(&mut self) {
+        todo!();
+    }
+
+    fn store(&mut self) {
+        todo!();
+    }
+
+    fn restore(&mut self) {
+        todo!();
     }
 }
