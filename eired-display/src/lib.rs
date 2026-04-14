81b30441fcd30eedbe3c5f19d43b220245415da8
TundraClimate <tundra.tundere@gmail.com>
2026-04-15 00:27:43 +0900

    📝docs: Add eired-display main docs

--- eired-display/src/lib.rs
+++ eired-display/src/lib.rs
@@ -1,3 +1,68 @@
+//! `eired-display` is low-layered terminal drawing crate.
+//!
+//! A core struct the [`VisualSpan`] is draw text to [`io::Write`](std::io::Write) with specified
+//! start location.
+//!
+//! The [`VisualSpan`] is execute these process as ansi escape.
+//! - Move cursor to expected location.
+//! - Writes text that composed by [`Cell`] array to the [`io::Write`](std::io::Write) impls but
+//!   not with flush.
+//! - Cursor stays at the end of text.
+//!
+//! ## Note
+//!
+//! This crate interprets the top left point as `(0, 0)`. since prevent `(0, 0)` and `(1, 1)` result
+//! to same by it uses `u16` to specify point for draw.
+//!
+//! ## Exmaple
+//!
+//! A simply example:  
+//! ```no_run
+//! use eired_display::{VisualSpan, Span};
+//! use std::io::{self, Write};
+//!
+//! # fn main() -> io::Result<()> {
+//! #
+//! let text = Span::from("That's exmaple text.");
+//!
+//! // Writes at cols:rows = 5:5
+//! let vs = VisualSpan::new((5, 5), text.as_slice());
+//!
+//! let mut stdout = io::stdout();
+//!
+//! // Draws stdout but not flushed
+//! vs.draw(&mut stdout)?;
+//!
+//! // Flushs stdout
+//! stdout.flush()?;
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! With styles:
+//! ```no_run
+//! use eired_display::{VisualSpan, Span, Style, AnsiColor};
+//! use std::io::{self, Write};
+//!
+//! # fn main() -> io::Result<()> {
+//! #
+//! // The text that has red foreground
+//! let text = Span::new("That's styled text.", Style::with_fg(AnsiColor::Red));
+//!
+//! // Writes at cols:rows = 5:5
+//! let vs = VisualSpan::new((5, 5), text.as_slice());
+//!
+//! let mut stdout = io::stdout();
+//!
+//! // Draws stdout but not flushed
+//! vs.draw(&mut stdout)?;
+//!
+//! // Flushs stdout
+//! stdout.flush()?;
+//! # Ok(())
+//! # }
+//! ```
+
 mod annot;
 mod cell;
 mod span;
